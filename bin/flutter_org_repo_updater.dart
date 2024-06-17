54ac99c8563c26cf731e060bda5c04fdb5ccc285
Narendran <narendran.i@xseededucation.com>
2024-06-17 14:06:31 +0530

    modify code

--- bin/flutter_org_repo_updater.dart
+++ bin/flutter_org_repo_updater.dart
@@ -5,23 +5,50 @@ import 'package:http/http.dart' as http;
 import 'package:yaml_edit/yaml_edit.dart';
 import 'package:process_run/shell.dart';
 
-String GITHUB_TOKEN = '';
-Future<void> loadGithubToken(List<String> arguments) async {
-  if (arguments.isNotEmpty) {
-    GITHUB_TOKEN = arguments[0];
-  } else {
-    final File tokenFile = File('github_token.txt');
-    if (await tokenFile.exists()) {
-      GITHUB_TOKEN = await tokenFile.readAsString();
-    } else {
-      print('GitHub token not provided and github_token.txt not found.');
-      return;
+Future<void> main(List<String> arguments) async {
+  final config = await loadConfig();
+  final orgPrefix = config['org_prefix'];
+  final orgUrl = config['org_url'];
+  final githubToken = config['github_token'];
+
+  if (orgPrefix == null && orgUrl == null) {
+    print('Either org_prefix or org_url must be provided in config.txt.');
+    return;
+  }
+
+  if (githubToken == null) {
+    print('github_token is not provided in config.txt.');
+    return;
+  }
+
+  await updateDependencies(orgPrefix, orgUrl, githubToken);
+}
+
+Future<Map<String, String>> loadConfig() async {
+  final File configFile = File('config.txt');
+  if (await configFile.exists()) {
+    final lines = await configFile.readAsLines();
+    final config = <String, String>{};
+    for (var line in lines) {
+      final parts = line.split('=');
+      if (parts.length == 2) {
+        config[parts[0].trim()] = parts[1].trim();
+      }
     }
+    return config;
+  } else {
+    print('config.txt not found.');
+    return {};
   }
 }
 
-void main(List<String> arguments) async {
-  await loadGithubToken(arguments);
+Future<void> runFlutterPubGet() async {
+  var shell = Shell();
+  await shell.run('flutter pub get');
+}
+
+Future<void> updateDependencies(
+    String? orgPrefix, String? orgUrl, String githubToken) async {
   final File file = File('pubspec.yaml');
   final String text = await file.readAsString();
   final dynamic yaml = loadYaml(text) as Map;
@@ -31,49 +58,54 @@ void main(List<String> arguments) async {
       jsonify(yaml['dependency_overrides']);
 
   for (var entry in dependencies.entries) {
-    await processXseedDependency(
-        entry.key, entry.value, editor, 'dependencies');
+    await processDependency(entry.key, entry.value, editor, 'dependencies',
+        orgPrefix, orgUrl, githubToken);
   }
   for (var entry in dependenciesOverrides.entries) {
-    await processXseedDependency(
-        entry.key, entry.value, editor, 'dependency_overrides');
+    await processDependency(entry.key, entry.value, editor,
+        'dependency_overrides', orgPrefix, orgUrl, githubToken);
   }
   await file.writeAsString(editor.toString());
 
   try {
     final File pubLock = File('pubspec.lock');
-
     pubLock.deleteSync();
   } catch (e) {
     print(e);
   }
 
-  var shell = Shell();
-  await shell.run('flutter pub get');
+  await runFlutterPubGet();
 }
 
 Map<String, dynamic> jsonify(dynamic value) {
   return value is String ? jsonDecode(value) : jsonDecode(jsonEncode(value));
 }
 
-Future<void> processXseedDependency(
-    String key, dynamic value, YamlEditor editor, String root) async {
-  if (isXseedDependency(key)) {
-    final xseedPackage = jsonify(value);
-    if (isProdPackage(xseedPackage)) {
-      final latestTag = await getLatestTag(xseedPackage['git']['url']);
-      if (latestTag != null && latestTag != xseedPackage['git']['version']) {
+Future<void> processDependency(String key, dynamic value, YamlEditor editor,
+    String root, String? orgPrefix, String? orgUrl, String githubToken) async {
+  if (isRelevantDependency(key, value, orgPrefix, orgUrl)) {
+    final package = jsonify(value);
+    if (isProdPackage(package)) {
+      final latestTag = await getLatestTag(package['git']['url'], githubToken);
+      if (latestTag != null && latestTag != package['git']['version']) {
         editor.update([root, key, 'git', 'version'], latestTag);
       }
     }
   }
 }
 
-Future<String?> getLatestTag(String repoUrl) async {
+bool isRelevantDependency(
+    String key, dynamic value, String? orgPrefix, String? orgUrl) {
+  final gitUrl = value['git']?['url'] ?? '';
+  return (orgPrefix != null && key.contains(orgPrefix)) ||
+      (orgUrl != null && gitUrl.contains(orgUrl));
+}
+
+Future<String?> getLatestTag(String repoUrl, String githubToken) async {
   final repoName = repoUrl.split('/').last.split('.').first;
-  final apiUrl = 'https://api.github.com/repos/xseededucation/$repoName/tags';
+  final apiUrl = 'https://api.github.com/repos/$repoName/tags';
   final headers = {
-    'Authorization': 'Bearer $GITHUB_TOKEN',
+    'Authorization': 'Bearer $githubToken',
     'Accept': 'application/vnd.github.v3+json',
     'X-GitHub-Api-Version': '2022-11-28',
   };
@@ -90,8 +122,4 @@ Future<String?> getLatestTag(String repoUrl) async {
 
 bool isProdPackage(Map<String, dynamic> json) {
   return json['git']?['version'] != null;
-}
-
-bool isXseedDependency(String key) {
-  return key.contains('xseed');
 }
